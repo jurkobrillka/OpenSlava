84cc2749e3c931e75f4175b2307c8e595259da00
jurkobrillka <brillaju@gmail.com>
2023-11-20 14:08:47 +0100

    Pridany text bakalarky

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        
+        System.out.println("Hello bakalarka");
     }
+
+
+
 
 }
